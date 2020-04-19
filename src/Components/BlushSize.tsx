9c8e9ad8632855805186dce4b0093aa034734f43
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2020-04-20 00:17:00 +0900

    Implement BlushSize.tsx

--- src/Components/BlushSize.tsx
+++ src/Components/BlushSize.tsx
@@ -1,27 +1,65 @@
 import React from 'react';
 import { flexSize } from '../typings/MaterialUI';
 import AnywherePaintContext from '../Contexts/AnywherePaintContext';
-import { Grid } from '@material-ui/core';
+import {
+  Grid,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  GridList,
+  GridListTile,
+} from '@material-ui/core';
 
 interface Props {
   ratio: flexSize;
 }
 
 interface State {
-  sizes: number[];
+  size: number;
 }
 
 class BlushSize extends React.Component<Props, State> {
   static contextType = AnywherePaintContext;
+  private sizes: number[] = [1, 2, 3, 4, 5, 7, 9, 11, 13, 15, 17, 20];
   constructor(props: Props) {
     super(props);
     this.state = {
-      sizes: [1, 2, 3, 4, 5, 7, 9, 11, 13, 15, 17, 20],
+      size: 1,
     };
   }
 
+  private handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const val = parseInt(e.target.value as string, 10);
+    this.setState({ size: val });
+    this.context.awPaint.setLineWidth(val);
+  }
+
   render() {
-    return <Grid item xs={this.props.ratio}></Grid>;
+    const blushSize = this.sizes.map((e) => (
+      <GridListTile key={e}>
+        <FormControlLabel
+          value={e}
+          label={`${e}px`}
+          labelPlacement="bottom"
+          control={<Radio color="primary" />}
+          key={e}
+        />
+      </GridListTile>
+    ));
+    return (
+      <Grid item xs={this.props.ratio}>
+        <GridList style={{ height: '80%' }}>
+          <RadioGroup
+            row
+            style={{ width: '100%' }}
+            value={this.state.size}
+            onChange={(e) => this.handleChange(e)}
+          >
+            {blushSize}
+          </RadioGroup>
+        </GridList>
+      </Grid>
+    );
   }
 }
 
