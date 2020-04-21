abd22f6b92ca887f14e3bb5f5d2994ffb497dc6e
tunamagur0 <47546832+tunamagur0@users.noreply.github.com>
2020-04-21 22:11:29 +0900

    Updates

---
+++ precache-manifest.7f955f42ebba2021ce598ddbaf6ec113.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf913abba7a58f87a8d55a8e04b778c3",
+    "url": "/anywhere-paint-sample/index.html"
+  },
+  {
+    "revision": "21e99b93e5be2b846d5f",
+    "url": "/anywhere-paint-sample/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f8a25e8b25f97c6fedf7",
+    "url": "/anywhere-paint-sample/static/js/2.17636f00.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/anywhere-paint-sample/static/js/2.17636f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e99b93e5be2b846d5f",
+    "url": "/anywhere-paint-sample/static/js/main.d4f4219a.chunk.js"
+  },
+  {
+    "revision": "ba0cd232810047839689",
+    "url": "/anywhere-paint-sample/static/js/runtime-main.f9376246.js"
+  }
+]);
